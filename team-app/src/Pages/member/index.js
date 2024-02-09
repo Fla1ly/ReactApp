aad6310f1b28ee1a9e5c3bdcc014f3141d57eef0
Dawid <musiald20@gmail.com>
2024-02-09 10:43:10 +0100

    updated styling

--- team-app/src/Pages/member/index.js
+++ team-app/src/Pages/member/index.js
@@ -1,27 +1,46 @@
 // MemberPage.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import teamMembers from '../../Components/Team/TeamMembers'; // Adjust the path accordingly
+import teamMembers from '../../Components/Team/TeamMembers';
+import Navbar from '../../Components/Navbar/Navbar';
+import '../../Assets/Styles/member.css';
 
 function MemberPage() {
   const { id } = useParams();
 
-  // Find the team member with the matching id
   const member = teamMembers.find((member) => member.id === parseInt(id, 10));
 
   if (!member) {
-    // Handle the case where the member is not found (optional)
     return <div>Member not found</div>;
   }
 
   return (
     <div>
-      <h1>{member.name}</h1>
-      <img src={member.profilePicture} alt={member.name} />
-      <p>Bio: {member.bio}</p>
-      <p>Email: {member.email}</p>
-      <p>Phone: {member.phone}</p>
-      {/* Add other member details as needed */}
+      <Navbar />
+      <div className='member-container'>
+        <div className='member-wrapper'>
+          <div className='member-left-container'>
+            <img src={member.profilePicture} alt={member.name} />
+          </div>
+          <div className='member-right-container'>
+            <h1>{member.name}</h1>
+            <h2>{member.title}</h2>
+            <div className='divider'></div>
+            <h3>{member.bio}</h3>
+            <div className='member-contact-info'>
+              <div className='member-icon-wrapper'>
+              <ion-icon name="mail-outline"></ion-icon><p>{member.email}</p>
+              </div>
+              <div className='member-icon-wrapper'>
+                <p><ion-icon name="call-outline"></ion-icon> {member.phone}</p>
+              </div>
+              <div className='member-icon-wrapper'>
+                <p><ion-icon name="pin-outline"></ion-icon> {member.location}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
